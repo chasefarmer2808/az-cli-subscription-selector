6ded44ec5ebb395262fa2037a07c10fe37d3eb99
cfarmer <cfarmer@fearless.tech>
2023-03-10 12:58:26 -0500

    get all subscriptions

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 #! /usr/bin/env node
 
-console.log("hello, world")
+import { exec } from 'child_process';
+
+function execShellCommand(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(error);
+      }
+      resolve(stdout ? stdout : stderr)
+    })
+  })
+}
+
+const accountListRaw = await execShellCommand('az account list');
+const subscriptions = JSON.parse(accountListRaw)
+console.log(subscriptions)
