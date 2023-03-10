9f118a4d3b86bf90a57c56c69a3950f454dcfea1
cfarmer <cfarmer@fearless.tech>
2023-03-10 13:10:01 -0500

    sets subscription

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
+import inquirer from 'inquirer';
 import { exec } from 'child_process';
 
 function execShellCommand(cmd) {
@@ -13,6 +14,22 @@ function execShellCommand(cmd) {
   })
 }
 
+async function setSubscription(subscriptionId) {
+  await execShellCommand(`az account set --subscription=${subscriptionId}`);
+}
+
 const accountListRaw = await execShellCommand('az account list');
-const subscriptions = JSON.parse(accountListRaw)
-console.log(subscriptions)
+const subscriptions = JSON.parse(accountListRaw);
+const subNames = subscriptions.map(sub => sub.name);
+
+const selectQuestion = {
+  type: 'list',
+  name: 'selectedSub',
+  message: 'Which subscription would you like to select?',
+  choices: subNames
+};
+
+const { selectedSub } = await inquirer.prompt([selectQuestion]);
+const selectedSubObj = subscriptions.find(sub => sub.name === selectedSub);
+await setSubscription(selectedSubObj.id);
+console.log(`Successfully set subscription to ${selectedSubObj.name} with ID ${selectedSubObj.id}`);
